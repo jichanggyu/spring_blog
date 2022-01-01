f61046f89e0e61207ab85faf900d368eeea4f9f6
jichanggyu <sa3590@naver.com>
2022-01-01 16:18:46 +0900

    객체를 가져오는 방법

--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,40 @@ public class DummyController {
 	
 	@Autowired // 의존성 주입 (DI)
 	private UserRepository userRepository;
+	
+	
+	// {id} 주소로 파라미터를 전달 받을 수 있음.
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id)	{
+		// user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될것아냐?
+		// 그럼 return할때 null이 리턴이 되자나... 그럼 프로그램에 문제가 있지 않겟니?
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return
+		
+		
+//		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+//			@Override
+//			public IllegalArgumentException get() {
+//				// TODO Auto-generated method stub
+//				return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
+//			}
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
+		});
+				
+		// get으로하는 잘못된 방법
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+//			@Override
+//			public User get() {
+//				// TODO Auto-generated method stub
+//				return new User();
+//			}
+//		});
+
+		return user;
+	}
 	
 	// http://localhost:8000/blog/dummy/join(요청)
 	// http body에 username,password,email 데이터를 가지고 (요청)
